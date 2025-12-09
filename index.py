fad99cd3cc0e40d0b1e110d8c70ae26936ccb437
rohan00112233 <microsofrohan1234@gmail.com>
2025-12-09 15:05:39 +0530

    FEAT: Add calculate_average function

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
 # index.py
 print("Hello, world!")
 print("Starting application...")
+
+def calculate_average(a, b):
+    return (a + b) / 2
+
+print("Average of 10 and 20 is:", calculate_average(10, 20))
